1f4234f118c2a4e86a9fcd067816f6f2209d2f8a
yueban <fbzhh007@gmail.com>
2025-10-18 16:40:42 +0800

    feat: initialize kotlin multiplatform compose project

---
+++ composeApp/src/iosMain/kotlin/com/yueban/compilecook/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.yueban.compilecook
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ composeApp/src/webMain/kotlin/com/yueban/compilecook/main.kt
@@ -0,0 +1,11 @@
+package com.yueban.compilecook
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport {
+        App()
+    }
+}
