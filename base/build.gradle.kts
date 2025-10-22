a974d2346888f30f132e39fa7472540df3e01ba5
yueban <fbzhh007@gmail.com>
2025-10-22 17:32:36 +0800

    refactor: remove kotlin/js target and related source files from all modules

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -33,11 +33,6 @@ kotlin {
 
   jvm()
 
-  js {
-    browser()
-    binaries.executable()
-  }
-
   @OptIn(ExperimentalWasmDsl::class)
   wasmJs {
     browser()
--- composeApp/src/jsMain/kotlin/com/yueban/compilecook/Platform.js.kt
+++
@@ -1,9 +0,0 @@
-@file:Suppress("MatchingDeclarationName")
-
-package com.yueban.compilecook
-
-class JsPlatform : Platform {
-  override val name: String = "Web with Kotlin/JS"
-}
-
-actual fun getPlatform(): Platform = JsPlatform()
