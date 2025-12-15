319633eaa6044047667b0b93616fbc35c5dc5cc8
yueban <fbzhh007@gmail.com>
2025-12-15 15:46:20 +0800

    feat: add back button handling with escape key in JVM main window

--- composeApp/src/jvmMain/kotlin/com/yueban/compilecook/main.kt
+++ composeApp/src/jvmMain/kotlin/com/yueban/compilecook/main.kt
@@ -2,11 +2,16 @@
 
 package com.yueban.compilecook
 
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.type
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.decompose.extensions.compose.lifecycle.LifecycleController
+import com.arkivanov.essenty.backhandler.BackDispatcher
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import com.yueban.compilecook.ui.root.DefaultRootComponent
 import com.yueban.compilecook.util.runOnUiThread
@@ -15,8 +20,15 @@ fun main() {
   AppInitializer.init()
 
   val lifecycle = LifecycleRegistry()
+  val backDispatcher = BackDispatcher()
+
   val root = runOnUiThread {
-    DefaultRootComponent(componentContext = DefaultComponentContext(lifecycle = lifecycle))
+    DefaultRootComponent(
+      componentContext = DefaultComponentContext(
+        lifecycle = LifecycleRegistry(),
+        backHandler = backDispatcher,
+      )
+    )
   }
 
   application {
@@ -25,6 +37,13 @@ fun main() {
 
     Window(
       onCloseRequest = ::exitApplication,
+      onKeyEvent = { event ->
+        if ((event.key == Key.Escape) && (event.type == KeyEventType.KeyUp)) {
+          backDispatcher.back()
+        } else {
+          false
+        }
+      },
       state = windowState,
       title = "CompileCook",
     ) {
