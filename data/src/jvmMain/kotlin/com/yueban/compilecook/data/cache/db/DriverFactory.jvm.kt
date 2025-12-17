e90ddd489d0cbaae4fd6c390bbb6a890771b646d
yueban <fbzhh007@gmail.com>
2025-12-17 18:12:12 +0800

    feat: create database file based on operating system for jvm target

--- data/src/jvmMain/kotlin/com/yueban/compilecook/data/cache/db/DriverFactory.jvm.kt
+++ data/src/jvmMain/kotlin/com/yueban/compilecook/data/cache/db/DriverFactory.jvm.kt
@@ -4,9 +4,22 @@ import app.cash.sqldelight.db.QueryResult.AsyncValue
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.db.SqlSchema
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import com.yueban.compilecook.BuildKonfig
+import com.yueban.compilecook.logger.Logger
+import net.harawata.appdirs.AppDirsFactory
+import java.io.File
 
-/**
- * TODO: populate dbFileName based on operating system.
- */
 actual fun provideDbDriver(schema: SqlSchema<AsyncValue<Unit>>, dbFileName: String): SqlDriver =
-  JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY).also { schema.create(it) }
+  JdbcSqliteDriver("jdbc:sqlite:${getDbFile(dbFileName).absolutePath}").also { schema.create(it) }
+
+private fun getDbFile(dbFileName: String) =
+  AppDirsFactory.getInstance()
+    .getUserDataDir(BuildKonfig.APP_NAME, BuildKonfig.APP_VERSION, null)
+    .let { File(it) }
+    .also {
+      if (!it.exists()) it.mkdirs()
+      Logger.d("userDataDir: $it")
+    }
+    .let {
+      File(it, dbFileName)
+    }
